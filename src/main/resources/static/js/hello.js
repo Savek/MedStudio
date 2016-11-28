7a38eb384d60d41adb5fc50e4bc2e5d824e4806e
Savek <krzysztof.mazur94@gmail.com>
2016-11-28 23:15:51 +0100

    UPDATE

--- src/main/resources/static/js/hello.js
+++ src/main/resources/static/js/hello.js
@@ -1,4 +1,4 @@
-angular.module('main', [ 'ngRoute', 'ngAnimate' ])
+angular.module('main', [ 'ngRoute', 'ngAnimate', 'ui.bootstrap' ])
     .config(function($routeProvider, $httpProvider) {
 
         $routeProvider.when('/', {
@@ -17,51 +17,52 @@ angular.module('main', [ 'ngRoute', 'ngAnimate' ])
 
         $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
 
-    })
-    .controller('navigation',
+    }).controller('navigation',
+    function($rootScope, $http, $location) {
 
-        function($rootScope, $http, $location) {
+        $rootScope.isNavCollapsed = true;
 
-            var self = this;
+        var self = this;
 
-            var authenticate = function(credentials, callback) {
+        var authenticate = function(credentials, callback) {
 
-                var headers = credentials ? {authorization : "Basic "
-                + btoa(credentials.username + ":" + credentials.password)
-                } : {};
+            var headers = credentials ? {authorization : "Basic "
+            + btoa(credentials.username + ":" + credentials.password)
+            } : {};
 
-                $http.get('user', {headers : headers}).then(function(response) {
-                    if (response.data.name) {
-                        $rootScope.authenticated = true;
-                        $location.path("/");
-                    } else {
-                        $rootScope.authenticated = false;
-                    }
-                    callback && callback();
-                }, function() {
-                    $rootScope.authenticated = false;
-                    callback && callback();
-                });
-
-            }
-
-            authenticate();
-            self.credentials = {};
-            self.login = function() {
-                authenticate(self.credentials, function() {
-                    if ($rootScope.authenticated) {
-                        $location.path("/");
-                        self.error = false;
-                    } else {
-                        $location.path("/login");
-                        self.error = true;
-                    }
-                });
-            };
-            self.logout = function() {
-                $http.post('logout', {}).finally(function () {
-                    $rootScope.authenticated = false;
+            $http.get('user', {headers : headers}).then(function(response) {
+                if (response.data.name) {
+                    $rootScope.authenticated = true;
                     $location.path("/");
-                });
-            }
-        });
+                } else {
+                    $rootScope.authenticated = false;
+                }
+                callback && callback();
+            }, function() {
+                $rootScope.authenticated = false;
+                callback && callback();
+            });
+
+        }
+
+        authenticate();
+        self.credentials = {};
+        self.login = function() {
+            authenticate(self.credentials, function() {
+                if ($rootScope.authenticated) {
+                    $location.path("/");
+                    self.error = false;
+                } else {
+                    $location.path("/login");
+                    self.error = true;
+                }
+            });
+        };
+        self.logout = function() {
+            $http.post('logout', {}).finally(function () {
+                $rootScope.authenticated = false;
+                $location.path("/");
+            });
+        }
+    }
+)
